8243e3811357bfa533abab551dfaed41b083d0b3
Azergharbi <92827794+Azergharbi@users.noreply.github.com>
2023-11-16 17:10:41 +0100

    Update course.service.ts

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -10,17 +10,17 @@ import { Observable } from 'rxjs';
 })
 export class CourseService {
 
-  private baseUrl = 'http://192.168.1.144:8089/api/course'; // Update the URL as per your backend
+  private baseUrl = '192.168.33.10:8090/api/course'; // Update the URL as per your backend
 
   constructor(private http: HttpClient) { }
 
   addCourse(course: Course): Observable<Course> {
-    const url = `${this.baseUrl}/add-course`;
+    const url = `${this.baseUrl}/add`;
     return this.http.post<Course>(url, course);
   }
 
   getAllCourses(): Observable<Course[]> {
-    const url = `${this.baseUrl}/retrieve-all-courses`;
+    const url = `${this.baseUrl}/all`;
     return this.http.get<Course[]>(url);
   }
 }
